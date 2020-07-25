ec02319430fda9a4811536bccb0b2e35fe1002b1
EduardoJM <eduardo_y05@outlook.com>
2020-07-25 20:27:05 -0300

    Testing changes in export

--- src/Phoria/index.ts
+++ src/Phoria/index.ts
@@ -1,44 +1,8 @@
-import CanvasRenderer from './CanvasRenderer';
-import Scene, { CameraHandler, TriggerHandler } from './Scene';
-import {
-    BaseEntity,
-    SceneHandler,
-    BeforeSceneHandler,
-    EntityStyle,
-    EntityStyleOptional,
-    Entity,
-    RenderHandle,
-    BaseLight,
-    DistantLight,
-    PointLight,
-} from './Entity';
-import { Edge, Polygon } from './Interfaces';
-import MeshFactory from './MeshFactory';
-import ImagePreLoader from './ImagePreLoader';
-import View, { MouseTrackingInstance } from './View';
-
-export {
-    Scene,
-    CameraHandler,
-    TriggerHandler,
-    CanvasRenderer,
-    // Entities
-    BaseEntity,
-    SceneHandler,
-    BeforeSceneHandler,
-    EntityStyle,
-    EntityStyleOptional,
-    Entity,
-    RenderHandle,
-    BaseLight,
-    DistantLight,
-    PointLight,
-    // Interfaces
-    Edge,
-    Polygon,
-    // Utils
-    MeshFactory,
-    ImagePreLoader,
-    View,
-    MouseTrackingInstance,
-};
+export * from './CanvasRenderer';
+export * from './Scene';
+export * from './Entity';
+export * from './Interfaces';
+export * from './MeshFactory';
+export * from './ImagePreLoader';
+export * from './View';
+export * from './Utils';
